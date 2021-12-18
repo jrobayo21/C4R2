7f2898db1e9b54014648a04ed435b9d1181120b1
Jhon Robayo <jhon.robayo.mt@correo.usa.edu.co>
2021-12-18 03:27:41 -0500

    actualizado front

--- front/js/registro.js
+++ front/js/registro.js
@@ -1,97 +1,106 @@
-function limpiarErrores(){
-    var error = document.getElementsByTagName('span');
-    for(var i=0; i<error.length; i++){
-        error[i].innerText="";
-    }
+$( document ).ready(function() {
+    console.log( "Estas en la página Registro" );
+    init();
+});
+function init()
+{
+	
 }
-var correo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-var patronNumeros = /[0-9]/;
 
-/*---------------------------------------
-            Registro
-----------------------------------------*/
-function validarNewUser(form){
-    limpiarErrores();
-    //validar nombre
-    if(form.nombre.value.trim().length==0){
-        document.getElementById('errorNombre').innerText="* Campo Obligatorio";
-        form.nombre.focus();
-        return false
-    }else if(patronNumeros.test(form.nombre.value)){
-        document.getElementById("errorNombre").innerText="* El nombre no puede contener números"
-        return false;
-    }
+function registrarUsuario(){
+	
+    var name = $.trim($("#name").val());
+    var email = $.trim($("#email").val());
+    var password = $.trim($("#password").val());
+	var password_r = $.trim($("#pwr_r").val());
+    
 
-    //validar email
-    if(!correo.test(form.emailN.value)){
-        document.getElementById('myEmail').innerText="* Email no valido";
-        form.emailN.focus;
-        return false;
-    }else if(form.emailN.value.trim().length==0){
-        document.getElementById('myEmail').innerText="* Campo obligatorio";
-        form.emailN.focus;
-        return false;
+    let myData = {
+		
+		 		name:name,
+                email:email,
+                password:password
+               
     }
-    $("#registro").on("blur", "#emailN", function () {
-        console.log("Ingreso")
-        console.log($(this).val())
+    let dataToSend = JSON.stringify(myData);
+
+                ///// Se crea usuario
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json",
+                    url: "http://localhost:8080/api/user/new",
+                    data: dataToSend,
+                    datatype: "json",
+                    cache: false,
+                    timeout: 600000,
+                    success: function(respuesta) {
+                        location.reload();
+						
+						
+                    },
+                    error: function(e) {
+                        alert("No FUNCIONA");
+                    },
+                    done: function(e) {
+                        alert("No FUNCIONA");
+                    }
+                });
+
+}
+
+
+function login()
+{
+    console.log("Mi boton Login Funciona");
+    var usuarioLogin = $.trim($("#email").val());
+    var usuarioPassword = $.trim($("#pwd").val());
+   console.log("usuario= " + usuarioLogin);
+    console.log("Password= " + usuarioPassword);
+    if (usuarioLogin != "" && usuarioPassword != "") {
+
         $.ajax({
-            url:"http://129.146.198.67:8080/api/user/"+$(this).val(),
-		     //"http://localhost:8080/api/user/"+$(this).val(),
+            url: "http://localhost:8080/api/user/" + usuarioLogin + "/" + usuarioPassword,
             type: "GET",
-            datatype:"JSON",
-            success:function(respuesta){
-                console.log(respuesta);
-                if(respuesta){
-                    document.getElementById('myEmail').innerText="* El Email ya esta registrado";
-                    form.email.focus;
-                    return false;
+            datatype: "JSON",
+            success: function(respuesta) {
+                //console.log(respuesta);
+                console.log("id " + respuesta.id);
+                if (respuesta.id === null) {
+                    alert("El usuario no existe o el password es incorrecto");
+                } else {
+                    console.log("ENTRO");
+                    validarperfil(respuesta.type);
+                    sessionStorage.setItem('miUser', JSON.stringify(respuesta));
+					location.href='perfilAdmin.html';
+
                 }
             }
-        })
-    })
+        });
+    } else { alert("Ningun campo debe estar vacio") }
 
-    //validar password
-    if(form.newpass.value.trim().length == 0){
-        document.getElementById('mypass').innerText='* Campo obligatorio';
-        form.newpass.focus();
-        return false;
-    }else if(form.newpass.value.trim().length < 7){
-        document.getElementById('mypass').innerText='* Clave minimo 7 caracteres';
-        form.newpass.focus();
-        return false;
-
-    }
-    //confirmar password
-    if(form.newpass.value != form.chekpass.value){
-        document.getElementById("confirmar").innerText="* La contraseña no coincide";
-        form.chekpass.focus();
-        return false;
-    }
-    jQuery(document).on("submit", "#registro", function(event){
-        event.preventDefault();
-        let info={
-            email: $($("#registro")[0].emailN).val(),
-            password: $($("#registro")[0].newpass).val(),
-            name: $($("#registro")[0].nombre).val()
-        }
-        data = JSON.stringify(info);
-        console.log(data);
-        $.ajax({
-            url:"http://129.146.198.67:8080/api/user/new",
-		     //"http://localhost:8080/api/user/new",
-            type:"POST",
-            datatype:"JSON",
-            contentType:"application/JSON; charset=utf-8",
-            data:data,
-            success: function(datos){
-                console.log(datos);
-                $(".ok").html("<span>Se creo la cuenta correctamente</span>").slideDown('slow');
-                setTimeout(function(){
-                    $(".ok").slideUp("slow");
-                },3000)
-            }
-        })
-
-    })
 }
+
+function validarperfil(perfil) {
+    switch (perfil) {
+        case 'ADM':
+            console.log('Perfil Admin');
+            window.location.href = "perfilAdmin.html";
+            break;
+        case 'COORD':
+            console.log('Perfil Coordinador');
+            break;
+        case 'ASE':
+            console.log('Perfil Asesor');
+            break;
+    }
+
+
+}
+$(document).on("click", ".btnLogin", function() {
+    login();
+	
+});
+
+$(document).on("click",".btnRegistrarse",function() {
+    registrarUsuario();
+});
